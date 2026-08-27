e2ebca855f8b0f5b0986e9120d9abd0e9f0b8d11
AndreyFinyak <afinyak@yahoo.com>
2026-08-28 02:06:01 +0300

    feat(API): Добавлены зависимости в dependencies.py для роутеров

---
+++ app/api/v1/dependencies.py
@@ -0,0 +1,22 @@
+from typing import AsyncGenerator
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.db.db_helper import db_helper
+from app.security import require_api_key
+from app.services.payment_service import PaymentService
+
+
+async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
+    async with db_helper.session_factory() as session:
+        yield session
+
+
+async def get_payment_service(
+        db: AsyncSession = Depends(get_db_session)
+) -> PaymentService:
+    return PaymentService(db=db)
+
+
+RequireApiKey = Depends(require_api_key)
